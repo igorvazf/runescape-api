1cf0928f9f70865c6a9a7b99ab40acefb644f325
Igor Fernandes <igorv1286@gmail.com>
2026-02-08 17:54:01 -0300

    client changes

--- runescape_api/rs3/client.py
+++ runescape_api/rs3/client.py
@@ -3,18 +3,14 @@ from runescape_api.rs3.endpoints.bestiary import Bestiary
 from runescape_api.rs3.endpoints.grand_exchange import GrandExchange
 from runescape_api.rs3.endpoints.hiscores import Hiscores
 from runescape_api.rs3.endpoints.runemetrics import Runemetrics
-from runescape_api.rs3.http.base_http_adapter import BaseHttpAdapter
 from runescape_api.rs3.http.rest_adapter import RestAdapter
 
 class RS3Client:
-    def __init__(self, timeout: int = 30, _http_adapter: Optional[BaseHttpAdapter] = None):
+    def __init__(self, timeout: int = 30):
         """
         Entry point for the RuneScape 3 API client.
-
-        The HTTP adapter is considered an internal dependency.
-        It may be overridden for testing purposes.
         """
-        self.http_adapter = _http_adapter or RestAdapter(timeout=timeout)
+        self.http_adapter = RestAdapter(timeout=timeout)
 
         self.bestiary = Bestiary(self.http_adapter)
         self.ge = GrandExchange(self.http_adapter)
